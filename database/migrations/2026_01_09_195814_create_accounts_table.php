f442c9814677917748bededaa86408610f759549
sayahosea <sayahosea@proton.me>
2026-01-10 03:08:02 +0700

    Add account table migration

---
+++ database/migrations/2026_01_09_195814_create_accounts_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->smallInteger('id', true)->primary();
+            $table->string('name', 60);
+            $table->string('email')->unique();
+            $table->string('password', 128);
+            $table->enum('role', ['admin', 'staff'])->default('staff');
+
+            $table->index(['id', 'email']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
